d7ba33f9f4278e71b1522caf0377cc289663c5de
zeynebG <memmedovazeyneb143@gmail.com>
2023-05-14 14:47:00 +0300

    final

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-const btn= document.querySelector(".learnMore")
+var btn = document.querySelector(".learnMore");
+var appBtn = document.querySelector(".appoinment");
+var contactUs = document.querySelector(".btn");
 
-function btntClick(){
-
-console.log("HEllo");
-
+function btnClick() {
+  console.log("hello");
+  window.location.href = "services.html";
 }
+btn.addEventListener("click", btnClick);
+
+function handleClick() {
+  console.log("hello");
+  window.location.href = "contact.html";
+}
+appBtn.addEventListener("click", handleClick);
+
+function eventClick() {
+  var inputs = document.querySelectorAll(".formPart input[required]");
+  var filled = true;
+
+  for (var i = 0; i < inputs.length; i++) {
+    if (inputs[i].value === "") {
+      filled = false;
+      break;
+    }
+  }
+
+  if (filled) {
+    alert("You registered successfully");
+  } else {
+    alert("Please fill the form");
+  }
+}
+inputs.addEventListener("click", eventClick);
+
+function clicking() {
+  window.location.href = "contact.html";
+}
+contactUs.addEventListener("click", clicking);
